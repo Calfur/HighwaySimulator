7546ef223a0d1a6e00fdb033c2ba7c00694faad4
Calfur <robin.nater@hotmail.com>
2023-01-01 23:55:50 +0100

    Refactor

--- App/src/app.ts
+++ App/src/app.ts
@@ -1,10 +1,9 @@
 import P5 from "p5";
-import Car from "./Car";
 import ConfigurationHandler from "./ConfigurationHandler";
 import Highway from "./Highway";
-import HighwayPosition from "./HighwayPosition";
-import "./styles.scss";
 import TrafficCalculator from "./TrafficCalculator";
+import "./styles.scss";
+import Car from "./Car";
 
 const sketch = (p5: P5) => {
 
@@ -22,27 +21,31 @@ const sketch = (p5: P5) => {
       var seconds = configurationHandler.TimeInSeconds();
       var timeCars = trafficCalculator.getClosestStateAtTime(seconds);
 
-      var mapPosition = p5.createVector(0, 0);
       var mapSize = p5.createVector(canvasWidth, canvasHeight);
-      var mapXInMeters = configurationHandler.MapXInMeters();
-      var amountOfLanes = 2;
-
-      var map = new Highway(
-         p5,
-         mapPosition,
-         mapSize,
-         mapXInMeters,
-         amountOfLanes,
-         timeCars.cars
-      );
-
-      map.draw();
+      drawMap(p5, mapSize, configurationHandler, timeCars.cars);
 
       addTextForCurrentlyShownSecond(p5, timeCars.seconds, canvasHeight);
    };
 };
 
 new P5(sketch);
+
+function drawMap(p5: P5, mapSize: P5.Vector, configurationHandler: ConfigurationHandler, cars: Car[]) {
+   var mapPosition = p5.createVector(0, 0);
+   var mapXInMeters = configurationHandler.MapXInMeters();
+   var amountOfLanes = 2;
+
+   var map = new Highway(
+      p5,
+      mapPosition,
+      mapSize,
+      mapXInMeters,
+      amountOfLanes,
+      cars
+   );
+
+   map.draw();
+}
 
 function addTextForCurrentlyShownSecond(p5: P5, seconds: number, canvasHeight: number) {
    p5.push();
