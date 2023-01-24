dfec3daeafc91209112a2f37cf15a9225a4d7855
Calfur <robin.nater@hotmail.com>
2023-01-24 13:34:20 +0100

    Update highway configuration

--- App/src/HighwaySimulator/Lane.ts
+++ App/src/HighwaySimulator/Lane.ts
@@ -1,29 +1,29 @@
 export default class Lane {
-    private _id: number;
-    private _maxSpeed: number;
-    private _beginning: number;
-    private _end: number;
- 
-    constructor(id:number, maxSpeed: number, beginning?:number, end?:number) {
-       this._id = id;
-       this._maxSpeed = maxSpeed;
-       this._beginning = beginning;
-       this._end = end;
-    }
+   private _id: number;
+   private _maxSpeed: number;
+   private _beginning: number;
+   private _end: number;
 
-    public get id() {
-        return this._id;
-    }
-  
-    public get maxSpeed() {
-       return this._maxSpeed;
-    }
-  
-    public get beginning() {
-       return this._beginning;
-    }
+   constructor(id: number, maxSpeed: number, beginning?: number, end?: number) {
+      this._id = id;
+      this._maxSpeed = maxSpeed;
+      this._beginning = beginning;
+      this._end = end;
+   }
 
-    public get end() {
-       return this._end;
-    }
+   public get id() {
+      return this._id;
+   }
+
+   public get maxSpeed() {
+      return this._maxSpeed;
+   }
+
+   public get beginning() {
+      return this._beginning;
+   }
+
+   public get end() {
+      return this._end;
+   }
 }
