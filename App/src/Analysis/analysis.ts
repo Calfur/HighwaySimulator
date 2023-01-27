42d97cbedfa9628ed6b12d527d6cdcee60c2c6a3
Calfur <robin.nater@hotmail.com>
2023-01-27 11:16:55 +0100

    Link json

--- App/src/Analysis/analysis.ts
+++ App/src/Analysis/analysis.ts
@@ -4,6 +4,7 @@ import JSONHandler from "../HighwaySimulator/JSONConfigHandler";
 import TrafficCalculator from "../HighwaySimulator/TrafficCalculator";
 import Chart from 'chart.js/auto';
 import { Spinner } from 'spin.js';
+import "./analysis.scss";
 
 const sketch = (p5: P5) => {
 	p5.setup = () => {
